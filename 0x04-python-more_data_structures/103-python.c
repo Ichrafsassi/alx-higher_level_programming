1f986749c314823c14851cb2a2c224b59a5a69f1
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-06-14 19:58:13 -0700

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <Python.h>
+struct timespec;
 
 /**
  * print_python_bytes - Prints bytes information
@@ -67,3 +68,4 @@ void print_python_list(PyObject *p)
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
 	}
+}
