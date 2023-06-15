706211b9e8d21fbb62d2003bfb2ffa5582fe85da
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-06-14 19:23:17 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,11 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
-    
+
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
     r_list = []
     res = 0
-    
+
     for c in roman_string:
         r_list.append(rom_n.get(c))
 
