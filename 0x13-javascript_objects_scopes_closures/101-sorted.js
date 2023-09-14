9045b1ce1cc8523fe2864fe26cc67bfe558eae6e
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-09-13 17:01:56 -0700

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 const dict = require('./101-data.js').dict;
-const New_dict = {};
+const newDict = {};
 
 for (const [key, value] of Object.entries(dict)) {
-  if (New_dict[value] === undefined) {
-    New_dict[value] = [key];
+  if (newDict[value] === undefined) {
+    newDict[value] = [key];
   } else {
-    New_dict[value].push(key);
+    newDict[value].push(key);
   }
 }
 
-console.log(New_dict);
+console.log(newDict);
