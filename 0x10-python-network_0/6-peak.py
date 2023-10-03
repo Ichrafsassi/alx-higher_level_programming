6102b355341762d2e0e45aab02e68eaca443630e
Ichrafsassi <ichraf.sassi.456@gmail.com>
2023-10-02 19:58:14 -0700

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,5 +4,18 @@
 
 def find_peak(list_of_integers):
     """list of unsorted integers"""
-    list_of_integers = sorted(list_of_integers)
-    return list_of_integers[-1]
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
